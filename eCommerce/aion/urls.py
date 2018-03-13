4a9accb7a22b66a387f2b8e40159b02a4a0d6a12
Joe <joecatarata@gmail.com>
2018-03-13 20:52:39 +0800

    Added Product Manager and Accounting Manager

--- eCommerce/aion/urls.py
+++ eCommerce/aion/urls.py
@@ -5,13 +5,14 @@ from . import views
 urlpatterns = [
 #    path('', views.HomePageView.as_view(), name='home'),
     path('', views.HomeView.as_view(), name='home'),
+    path('home/', views.HomeView.as_view(), name='home'),
     path('category/<slug:category>/', views.CategoryView.as_view(), name='category'),
     path('search/', views.SearchView.as_view(), name='search'),
     path('login/', views.login_view, name='login'),
     path('addproduct/', views.CreateProductView.as_view(), name='addproduct'),
     path('product/<int:pk>/edit/', views.EditProductView.as_view(), name='editproduct'),
     path('product/<int:pk>/delete/', views.DeleteProductView.as_view(), name='deleteproduct'),
-    
+
     path('product/<int:pk>/', views.ViewProduct.as_view(), name='viewproduct'),
     path('user/<int:pk>/', views.ViewAccount.as_view(), name='viewaccount'),
     path('register/', views.UserFormView.as_view(), name='register'),
--- eCommerce/aion/views.py
+++ eCommerce/aion/views.py
@@ -9,7 +9,7 @@ from .models import *
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
-    
+
 class HomeView(generic.ListView):
     template_name = 'aion/home.html'
     context_object_name = 'products'
@@ -19,7 +19,7 @@ class HomeView(generic.ListView):
 #    def get_paginate_by(self, queryset):
 #        self.paginate_by = self.request.GET.get('paginate_by', self.paginate_by)
 #        return self.paginate_by
-#    
+#
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context["loggeduser"] = self.request.user
@@ -33,7 +33,7 @@ class UserFormView(generic.View):
     ship_form_class = AddressDetailsForm
     title = "Register"
     template_name = 'aion/register.html'
-    
+
     #display blank form
     def get(self, request):
         form1 = self.form_class(None)
@@ -41,53 +41,53 @@ class UserFormView(generic.View):
         form3 = self.bill_form_class(None)
         form4 = self.ship_form_class(None)
         return render(request, self.template_name,{'form1':form1,'form2':form2,'form3':form3, 'form4':form4,"title": self.title})
-    
+
     #process form data
     def post(self, request):
         form1 = self.form_class(request.POST)
         form2 = self.second_form_class(request.POST)
         form3 = self.bill_form_class(request.POST)
         form4 = self.ship_form_class(request.POST)
-        
+
         if form1.is_valid() and form2.is_valid() and form3.is_valid() and form4.is_valid():
-            
+
             user = form1.save(commit=False)
-            
+
             username = form1.cleaned_data['username']
             password = form1.cleaned_data['password']
             user.set_password(password)
             user.save()
-            
-            
+
+
             user_details = form2.save(commit=False)
             user_details.user_id = user
-            
+
             billing_address = form3.save()
             shipping_address = form4.save()
-            
+
             user_details.billing_address = billing_address
             user_details.shipping_address = shipping_address
             user_details.save()
-            
-            
+
+
             #return User objects if credentials are correct
             user = authenticate(username=username,password=password)
-            
+
             if user is not None:
-                
+
                 if user.is_active:
                     login(request,user)
                     return HttpResponseRedirect('/home/')
-            
-            
-                
+
+
+
         return render(request, self.template_name,{'form1':form1,'form2':form2,'form3':form3, 'form4':form4, "title": self.title})
 
 def login_view(request):
 	print(request.user)
 	title = "Login"
 	form = UserLoginForm(request.POST or None)
-    
+
 	if form.is_valid():
 		username = form.cleaned_data.get("username")
 		password = form.cleaned_data.get("password")
@@ -100,7 +100,7 @@ def login_view(request):
 class CreateProductView(CreateView):
     form_class = ProductForm
     template_name = 'addproduct.html'
-    
+
     def form_valid(self, form):
         form.instance.user_id = self.request.user
         return super(CreateProductView, self).form_valid(form)
@@ -109,11 +109,11 @@ class CreateProductView(CreateView):
 class EditProductView(generic.UpdateView):
     form_class = ProductForm
     template_name = 'addproduct.html'
-    
+
     def get_object(self, queryset=None):
         obj = Product.objects.get(id=self.kwargs['pk'])
         return obj
-    
+
     def get_context_data(self, **kwargs):
         context = super(EditProductView, self).get_context_data(**kwargs)
         context["loggeduser"] = self.request.user
@@ -123,27 +123,28 @@ class EditProductView(generic.UpdateView):
 #Delete Product
 class DeleteProductView(generic.DeleteView):
     model = Product
-    
+
     def get_object(self, queryset=None):
         obj = Product.objects.get(id=self.kwargs['pk'])
         return obj
-    
+
     def get_context_data(self, **kwargs):
         context = super(DeleteProductView, self).get_context_data(**kwargs)
         context["loggeduser"] = self.request.user
 #        context["object"] = Product.objects.get(id=self.kwargs['pk'])
         return context
-    
+
     def get_success_url(self):
-        # Assuming there is a ForeignKey from Comment to Post in your model 
+        # Assuming there is a ForeignKey from Comment to Post in your model
         return reverse('home')
 
-    
+
 class ViewProduct(generic.DetailView):
     model = Product
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["user_details"] = User_Details.objects.filter(user_id=self.request.user)
         context["loggeduser"] = self.request.user
         return context
 
@@ -154,16 +155,16 @@ class ViewAccount(generic.DetailView):
         context["user_details"] = User_Details.objects.get(user_id=self.object)
         context["user"] = self.object
         context["loggeduser"] = self.request.user
-        
+
         return context
-    
+
 class SearchView(generic.ListView):
     template_name = 'aion/home.html'
     context_object_name = 'products'
-    
+
     def get_queryset(self):
         return Product.objects.filter(item_name__icontains=self.request.GET.get("search_input", None)).order_by('-id')
-    
+
     def get_context_data(self, **kwargs):
         context = super(SearchView, self).get_context_data(**kwargs)
         context["loggeduser"] = self.request.user
@@ -172,26 +173,25 @@ class SearchView(generic.ListView):
 class CategoryView(generic.ListView):
     template_name = 'aion/home.html'
     context_object_name = 'products'
-    
+
     def get_queryset(self):
         return Product.objects.filter(item_type_slug=self.kwargs['category']).order_by('-id')
-    
+
     def get_context_data(self, **kwargs):
         context = super(CategoryView, self).get_context_data(**kwargs)
         context["loggeduser"] = self.request.user
-        
+
         return context
-    
+
 class CartView(TemplateView):
     template_name = 'aion/cart.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super(CartView, self).get_context_data(**kwargs)
         cart = Cart.objects.filter(user_id=self.request.user,isPurchased=False)
-        
+
         context["orders"] = Order.objects.filter(cart_id=cart)
         context["cart"] = cart
         context["loggeduser"] = self.request.user
-        
+
         return context
-    
