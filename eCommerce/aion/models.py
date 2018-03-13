50e8e08eb7bc3b1f1d8cee7626fb055ea1f3779d
Joe Catarata <joecatarata@gmail.com>
2018-03-13 16:27:31 +0800

    Removed extended nav bar and added basic role

--- eCommerce/aion/models.py
+++ eCommerce/aion/models.py
@@ -11,11 +11,11 @@ class Address_Details(models.Model):
     city = models.CharField(max_length=200)
     postal_code = models.IntegerField(default=0)
     country = models.CharField(max_length=200)
-    
+
     #(House #, Street, Subdivision, City, Postal Code, Country)
     def __str__(self):
         return '('+ self.house_number +', '+ self.street +', '+ self.subdivision +', '+ self.city +', '+ str(self.postal_code) +', '+ self.country +')'
-    
+
     class Meta:
         verbose_name_plural = "Address_Details"
 
@@ -26,22 +26,22 @@ class User_Details(models.Model):
     last_name = models.CharField(max_length=200)
     billing_address = models.ForeignKey(Address_Details, on_delete=models.CASCADE, related_name ="billing_address", blank=True,null=True)
     shipping_address = models.ForeignKey(Address_Details, on_delete=models.CASCADE,related_name ="shipping_address", blank=True,null=True)
-    
+
     type_choice = (
         ('Customer', 'Customer'),
         ('Product Manager', 'Product Manager'),
         ('Accounting Manager', 'Accounting Manager'),
         ('Administrator', 'Administrator'),
     )
-    
+
     account_type = models.CharField(max_length=50, choices=type_choice, default=type_choice[0][0])
-    
+
     def __str__(self):
         return str(self.user_id) +', '+ self.first_name +' '+ self.last_name
-    
+
     class Meta:
         verbose_name_plural = "User_Details"
-    
+
 
 class Product(models.Model):
     user_id = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -51,18 +51,18 @@ class Product(models.Model):
     item_quantity = models.IntegerField(default=0)
     item_price = models.DecimalField(default=0, max_digits=19, decimal_places=2)
     isPurchased = models.BooleanField(default=False)
-    
+
     type_choice = (
         ('Analog watch', 'Analog watch'),
         ('Digital watch', 'Digital watch'),
         ('Smart watch', 'Smart watch'),
     )
-    
+
     item_type = models.CharField(max_length=50, choices=type_choice, default=type_choice[0][0])
     item_type_slug = AutoSlugField(populate_from='item_type', blank=True)
-    
+
     def __str__(self):
         return self.item_name +' ('+str(self.id)+') '
-    
+
     def get_absolute_url(self):
         return reverse('home')
