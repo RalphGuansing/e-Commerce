a43e59cfad109ff8b0f1543ed8b7a4510fe0e915
Ralph Guansing <ralph_guansing@dlsu.edu.ph>
2018-03-14 01:38:48 +0800

    added commas to pricing

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'materialize',
     'crispy_forms',
     'crispy_forms_materialize',
